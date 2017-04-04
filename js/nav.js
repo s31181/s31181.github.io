f19ed1be7c9830f95d0c201fc98066a8fbd02429
Rob <robert.caluza01@gmail.com>
2017-04-03 22:16:31 -0500

    css update index update js update

--- js/nav.js
+++ js/nav.js
@@ -8,8 +8,22 @@ $(document).ready(function() {
   $(window).click(function() {
     $('.burger').removeClass('open');
     $('.nav').removeClass('slide_out');
+    console.log('i took a shit');
   });
   $('.burger-container').click(function(event) {
     event.stopPropagation();
+    console.log('holy crap');
   });
+
+  $('a[href^="#"]').on('click', function(event) {
+   event.preventDefault();
+   var target = this.hash;
+   var $target = $(target);
+   console.log($target);
+   $('body, html').animate({
+     'scrollTop': $target.offset().top
+   }, 1000, 'swing', function () {
+     window.location.hash = target;
+   });
+   });
 });
