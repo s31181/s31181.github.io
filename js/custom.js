b2f56566b8b0d69125139ded919ee3130f2c8c83
Rob <robert.caluza01@gmail.com>
2017-04-05 16:18:41 -0500

    one of many *final commits

--- js/custom.js
+++ js/custom.js
@@ -1,12 +1,23 @@
 $(document).ready(function() {
-  $('a[href^="#"]').on('click', function(e) {
-    e.preventDefault();
 
-    var target = this.hash;
-    var $target = $(target);
+  var aboutEl = $('.peek');
+  var skillEl = $('.pook');
+  var projEl = $('.jook');
+  var aboutElOffset = aboutEl.offset().top / 2;
+  var skillElOffset = skillEl.offset().top / 2;
+  var projElOffset = $('#projects').offset().top - 10;
+  var documentEl = $(document);
 
-    $('html, body').animate({
-      'scrollTop': $target.offset().top
-    }, 1000, 'swing');
-    });
+  documentEl.on('scroll',function(){
+    if (documentEl.scrollTop() > aboutElOffset && aboutEl.hasClass('peek') ) aboutEl.addClass('a_boo');
+  });
+
+  documentEl.on('scroll', function() {
+    if (documentEl.scrollTop() > skillElOffset && skillEl.hasClass('pook')) skillEl.addClass('a_boo');
+  });
+
+  documentEl.on('scroll', function() {
+    if (documentEl.scrollTop() > projElOffset && projEl.hasClass('jook')) projEl.addClass('a_boo');
+  });
+
 });
